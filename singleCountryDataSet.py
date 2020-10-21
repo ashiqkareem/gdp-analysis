bbace80aa1e20287296e71d3e494f622e456e33b
DweebBob <63865160+DweebBob@users.noreply.github.com>
2020-10-22 01:22:42 +0800

    Import excel files Add and change variable names

--- singleCountryDataSet.py
+++ singleCountryDataSet.py
@@ -14,27 +14,66 @@ import os
 userSelection = 'Singapore'
 
 # Dataframes
-data1 = pd.read_excel("../Datasets/SAMPLE - Agriculture, forestry, and fishing, value added (% of GDP).xlsx")
-data2 = pd.read_excel("../Datasets/SAMPLE - Arable land (% of land area).xlsx")
-data3 = pd.read_excel("../Datasets/SAMPLE - GDP per capita (constant LCU).xlsx")
-sgrow1 = data1.loc[data1['Country Name'] == userSelection]
-sgrow2 = data2.loc[data2['Country Name'] == userSelection]
-sgrow3 = data3.loc[data3['Country Name'] == userSelection]
+dataGDP = pd.read_excel('../gdp-analysis/Datasets/SAMPLE - GDP per capita (constant LCU).xlsx')
+dataAgri = pd.read_excel('../gdp-analysis/Datasets/SAMPLE - Agriculture, forestry, and fishing, value added (% of GDP).xlsx')
+dataArab = pd.read_excel('../gdp-analysis/Datasets/SAMPLE - Arable land (% of land area).xlsx')
+dataBirth = pd.read_excel('../gdp-analysis/Datasets/SAMPLE - Birth rate, crude (per 1,000 people).xlsx')
+dataDeath = pd.read_excel('../gdp-analysis/Datasets/SAMPLE - Death rate, crude (per 1,000 people).xlsx')
+dataIndiv = pd.read_excel('../gdp-analysis/Datasets/SAMPLE - Individuals using the Internet (% of population).xlsx')
+dataIndus = pd.read_excel('../gdp-analysis/Datasets/SAMPLE - Industry (including construction), value added (% of GDP).xlsx')
+dataMobile = pd.read_excel('../gdp-analysis/Datasets/SAMPLE - Mobile cellular subscriptions (per 100 people).xlsx')
+dataMort = pd.read_excel('../gdp-analysis/Datasets/SAMPLE - Mortality rate, infant (per 1,000 live births).xlsx')
+dataMig = pd.read_excel('../gdp-analysis/Datasets/SAMPLE - Net migration.xlsx')
+dataCrop = pd.read_excel('../gdp-analysis/Datasets/SAMPLE - Permanent cropland (% of land area).xlsx')
+dataPopDen = pd.read_excel('../gdp-analysis/Datasets/SAMPLE - Population density (people per sq. km of land area).xlsx')
+dataPop = pd.read_excel('../gdp-analysis/Datasets/SAMPLE - Population, total.xlsx')
+dataServ = pd.read_excel('../gdp-analysis/Datasets/SAMPLE - Services, value added (% of GDP).xlsx')
+dataArea = pd.read_excel('../gdp-analysis/Datasets/SAMPLE - Surface area (sq. km).xlsx')
+
+#Selecting row based on country selected
+rowGDP = dataGDP.loc[dataGDP['Country Name'] == userSelection]
+rowAgri = dataAgri.loc[dataAgri['Country Name'] == userSelection]
+rowArab = dataArab.loc[dataArab['Country Name'] == userSelection]
+rowBirth = dataBirth.loc[dataBirth['Country Name'] == userSelection]
+rowDeath = dataDeath.loc[dataDeath['Country Name'] == userSelection]
+rowIndiv = dataIndiv.loc[dataIndiv['Country Name'] == userSelection]
+rowIndus = dataIndus.loc[dataIndus['Country Name'] == userSelection]
+rowMobile = dataMobile.loc[dataMobile['Country Name'] == userSelection]
+rowMort = dataMort.loc[dataMort['Country Name'] == userSelection]
+rowMig = dataMig.loc[dataMig['Country Name'] == userSelection]
+rowCrop = dataCrop.loc[dataCrop['Country Name'] == userSelection]
+rowPopDen = dataPopDen.loc[dataPopDen['Country Name'] == userSelection]
+rowPop = dataPop.loc[dataPop['Country Name'] == userSelection]
+rowServ = dataServ.loc[dataServ['Country Name'] == userSelection]
+rowArea = dataArea.loc[dataArea['Country Name'] == userSelection]
 
 # Transposing columns and rows
-sgcol3 = sgrow3.T
-sgcol1 = sgrow1.T
-sgcol2 = sgrow2.T
-sgcol3 = sgrow3.T
+colGDP = rowGDP.T
+colAgri = rowAgri.T
+colArab = rowArab.T
+colBirth = rowBirth.T
+colDeath = rowDeath.T
+colIndiv = rowIndiv.T
+colIndus = rowIndus.T
+colMobile = rowMobile.T
+colMort = rowMort.T
+colMig = rowMig.T
+colCrop = rowCrop.T
+colPopDen = rowPopDen.T
+colPop = rowPop.T
+colServ = rowServ.T
+colArea = rowArea.T
 
 # Concatenating the different factors into 1 dataframe
-df = pd.concat([sgcol3, sgcol1, sgcol2], axis=1)
-df.columns = ['GDP', 'Agriculture', 'Arable Land']
+df = pd.concat([colGDP, colAgri, colArab], axis=1)
+df.columns = ['GDP', 'Agriculture', 'Arable Land', 'Birth Rate', 'Death Rate', 'Individual Internet usage', 'Industry',
+              'Mobile Subscriptions', 'Mortality Rate','Migration Net','Cropland','Population Density','Population',
+              'Services','Surface Area']
 df.drop(['Series Name', 'Series Code', 'Country Name', 'Country Code'], axis=0, inplace=True)
 df = pd.DataFrame(df, dtype=float)
 
 # Making of a line graph
-ax = sns.lineplot(x='Arable Land', y='GDP', data=df)
+ax = sns.lineplot(x='Agriculture', y='GDP', data=df)
 plt.show()
 
 # Correlation value of GDP vs factor (requires a for loop implementation)
