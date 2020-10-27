4fff76048e087c8aa1b9e86e6be160d2a79bee3b
Galaxy-power <su_jingxuan@hotmail.com>
2020-10-27 12:54:00 +0800

    Working with the raw data set, just need to edit the plot design

--- data.py
+++ data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 # Dataframes
+"""
 path ='../gdp-analysis/Datasets/'
 dataGDP = pd.read_excel(path + 'SAMPLE - GDP per capita (constant LCU).xlsx')
 dataAgri = pd.read_excel(path + 'SAMPLE - Agriculture, forestry, and fishing, value added (% of GDP).xlsx')
@@ -16,6 +17,24 @@ dataPopDen = pd.read_excel(path + 'SAMPLE - Population density (people per sq. k
 dataPop = pd.read_excel(path + 'SAMPLE - Population, total.xlsx')
 dataServ = pd.read_excel(path + 'SAMPLE - Services, value added (% of GDP).xlsx')
 dataArea = pd.read_excel(path + 'SAMPLE - Surface area (sq. km).xlsx')
+"""
+# Raw Data Set
+path ='../gdp-analysis/rawDataSet/'
+dataGDP = pd.read_csv(path + 'GDP per capita (constant LCU).csv')
+dataAgri = pd.read_csv(path + 'Agriculture, forestry, and fishing, value added (% of GDP).csv')
+dataArab = pd.read_csv(path + 'Arable land (% of land area).csv')
+dataBirth = pd.read_csv(path + 'Birth rate, crude (per 1,000 people).csv')
+dataDeath = pd.read_csv(path + 'Death rate, crude (per 1,000 people).csv')
+dataIndiv = pd.read_csv(path + 'Individuals using the Internet (% of population).csv')
+dataIndus = pd.read_csv(path + 'Industry (including construction), value added (% of GDP).csv')
+dataMobile = pd.read_csv(path + 'Mobile cellular subscriptions (per 100 people).csv')
+dataMort = pd.read_csv(path + 'Mortality rate, infant (per 1,000 live births).csv')
+dataCrop = pd.read_csv(path + 'Permanent cropland (% of land area).csv')
+dataPopDen = pd.read_csv(path + 'Population density (people per sq. km of land area).csv')
+dataPop = pd.read_csv(path + 'Population, total.csv')
+dataServ = pd.read_csv(path + 'Services, value added (% of GDP).csv')
+dataArea = pd.read_csv(path + 'Surface area (sq. km).csv')
+
 
 
 # Store files in list to select the files to show based on user input
--- showGDP.py
+++ showGDP.py
@@ -15,13 +15,31 @@ from sklearn.metrics import mean_squared_error, mean_squared_log_error
 from data import *
 
 # User input
-user_year = 1960
+user_year = "1960"
 user_factor ="Agriculture, forestry, and fishing, value added (% of GDP)"
+
+
+# Clean columns in data frame
+def clean_dataframe(frame):
+    frame.columns = frame.columns.str.replace(r"\[.*?\]", "")  # Remove Brackets
+    frame.rename(columns=lambda x: x.strip(), inplace=True)  # Strip the spaces
+    frame = frame.dropna()
+    # frame = frame.replace('..', np.nan).dropna()
+    # frame = frame.replace('...', np.nan).dropna()
+    return frame
 
 
 # Declare var and GDP dataframe
 num = 10
 df = pd.DataFrame(dataGDP)
+df = clean_dataframe(df)
+
+# Dropping columns that are not needed after cleaning based on user selection
+for column in df.columns[4:]:
+    if column != user_year:
+        df = df.drop(column, 1)
+df = df.replace('..', np.nan).dropna()
+df = df.replace('...', np.nan).dropna()
 
 
 # Create data frame base on factor user selected
@@ -29,38 +47,43 @@ def file_to_execute(file_to_show):
     for file in file_selection:
         if file["Series Name"][0] == file_to_show:
             df_2 = pd.DataFrame(file)
+            df_2 = clean_dataframe(df_2)
     return df_2
 
 
 # Setting the basic style for plots
 def plot_design(title):
     plt.title(title)
+    plt.style.use('ggplot')
     plt.xlabel("GPD")
     plt.ylabel("Countries")
-    plt.style.use('ggplot')
     plt.show()
 
 
+# Display GPD for all Countries based on year
 def display_all(user_input):
-    df[user_input] = df[user_input].astype(int)
     plt.barh(df['Country Name'], df[user_input])
     plot_design("GPD For All Countries")
 
 
+# Display GDP of top 10 Countries based on year
 def display_top_gpd(user_input):
     df.sort_values(by=[user_input, "Country Name"], inplace=True)
     plt.barh(df['Country Name'][-num:], df[user_input][-num:])
     plot_design("GPD Of Top Countries")
 
 
+# Display GDP of bottom 10 Countries based on year
 def display_btm_gpd(user_input):
     df.sort_values(by=[user_input, "Country Name"], inplace=True)
+    print(df[user_input])
     plt.barh(df['Country Name'][:num], df[user_input][:num])
     plot_design("GPD Of Bottom Countries")
 
 
+# Display top 10 countries of specific factor in specific year
 def display_specific_factor(user_input, factor_input):
-    x = file_to_execute(user_factor)
+    x = file_to_execute(factor_input)
     x.sort_values(by=[user_input, "Country Name"], inplace=True)
     plt.barh(x['Country Name'][-num:], x[user_input][-num:])
     plot_design(factor_input)
@@ -69,3 +92,6 @@ def display_specific_factor(user_input, factor_input):
 # display_top_gpd(user_year)
 # display_btm_gpd(user_year)
 # display_specific_factor(user_year,user_factor)
+
+
+# file_to_execute(user_factor)
