a1b563fcd025bc54c9cb258a515cef92b16eccb1
Galaxy-power <su_jingxuan@hotmail.com>
2020-10-29 00:37:19 +0800

    i change 2 function name the top 10 and btm 10 cause of the typo lol. Rest all working :)

--- showGDP.py
+++ showGDP.py
@@ -1,77 +1,99 @@
+"""This file contains the features for All Countries"""
+
+# Import all necessary modules
 import numpy as np
 import pandas as pd
 from pandastable import Table
 from pandastable import config
 from matplotlib import pyplot as plt
-from data import *
 import tkinter as tk
 from tkinter import *
 
+# RawDataSet
+path ="../gdp-analysis/rawDataSet/"
+dataGDP = pd.read_csv(path + 'GDP, PPP (current international $).csv')
+dataAgri = pd.read_csv(path + 'Agriculture, forestry, and fishing, value added (% of GDP).csv')
+dataArab = pd.read_csv(path + 'Arable land (% of land area).csv')
+dataBirth = pd.read_csv(path + 'Birth rate, crude (per 1,000 people).csv')
+dataDeath = pd.read_csv(path + 'Death rate, crude (per 1,000 people).csv')
+dataIndiv = pd.read_csv(path + 'Individuals using the Internet (% of population).csv')
+dataIndus = pd.read_csv(path + 'Industry (including construction), value added (% of GDP).csv')
+dataMobile = pd.read_csv(path + 'Mobile cellular subscriptions (per 100 people).csv')
+dataMort = pd.read_csv(path + 'Mortality rate, infant (per 1,000 live births).csv')
+dataCrop = pd.read_csv(path + 'Permanent cropland (% of land area).csv')
+dataPopDen = pd.read_csv(path + 'Population density (people per sq. km of land area).csv')
+dataPop = pd.read_csv(path + 'Population, total.csv')
+dataServ = pd.read_csv(path + 'Services, value added (% of GDP).csv')
+dataArea = pd.read_csv(path + 'Surface area (sq. km).csv')
 
-# User input
-# user_year = "1960"
-# user_factor ="Agriculture, forestry, and fishing, value added (% of GDP)"
+
+# Store the files into  a list
+file_selection = [dataGDP, dataAgri, dataArab, dataBirth, dataDeath, dataIndiv, dataIndus, dataMobile, dataMort,
+                  dataCrop, dataPopDen, dataPop, dataServ, dataArea]
 
 
-# Clean columns in data frame
+# Clean values in data frame
 def clean_dataframe(frame):
+    """
+    :param frame: Takes in a dataframe
+    Clean the data frame and remove any unwanted values
+    :return: return a clean data frame
+    """
     frame.columns = frame.columns.str.replace(r"\[.*?\]", "")  # Remove Brackets
     frame.rename(columns=lambda x: x.strip(), inplace=True)  # Strip the spaces
     frame = frame.dropna()
     return frame
 
 
-# Declare var and GDP dataframe
+# Declare variable and GDP dataframe
 num = 10
 df = pd.DataFrame(dataGDP)
 df = clean_dataframe(df)
 
-"""
-# Dropping columns that are not needed after cleaning based on user selection
-def drop_column(user_year):
-    for column in df.columns[4:]:
-        if column != user_year:
-            df = df.drop(column, 1)
-    df = df.replace('..', np.nan).dropna()
-    df = df.replace('...', np.nan).dropna()
-    df.drop(['Series Name', 'Series Code', 'Country Code'], axis=1, inplace=True)
 
-
-# Create data frame base on factor user selected
-def file_to_execute(file_to_show):
-    for file in file_selection:
-        if file["Series Name"][0] == file_to_show:
-            df_2 = pd.DataFrame(file)
-            df_2 = clean_dataframe(df_2)
-    return df_2
-"""
-
-# Setting the basic style for plots
-def plot_design(title):
-    plt.title(title)
-    plt.style.use('ggplot')
-    plt.xlabel("GPD")
-    plt.ylabel("Countries")
-    plt.tight_layout()
-    plt.show()
-
-
-# Display GPD for all Countries based on year
-def display_all(user_input):
-    df = pd.DataFrame(dataGDP)
-    df = clean_dataframe(df)
+# Trim columns that are not needed
+def trim_column(df,user_input):
+    """
+    :param df: Take in a data frame as input
+    :param user_input: Fetch year value from user input
+    :return: Dataframe with trim columns
+    """
     for column in df.columns[4:]:
         if column != user_input:
             df = df.drop(column, 1)
-
     df = df.replace('..', np.nan).dropna()
     df = df.replace('...', np.nan).dropna()
-    df.drop(['Series Name', 'Series Code', 'Country Code'], axis=1, inplace=True)
+    return df
+
+
+# Setting the basic style for plots
+def plot_design(title):
+    """
+    :param title: Main title for plot graph
+    Add a standard style for all graphs
+    """
+    plt.title(title)
+    plt.xlabel("GDP")
+    plt.ylabel("Countries")
+    plt.tight_layout()
+    plt.xticks(rotation='15')
+    plt.show()
+
+
+# Display GDP for all Countries based on year
+def display_all(user_input):
+    """
+    :param user_input: Fetch year value from user input
+    Generate DataFrame based on factor value and plot accordingly
+    """
+    new_df = trim_column(df, user_input)
+    new_df.drop(['Series Name', 'Series Code', 'Country Code'], axis=1, inplace=True)
+    new_df.reset_index(drop=True,inplace=True)
     window = tk.Toplevel()
     window.title('GPD For All Countries')
     f = Frame(window)
     f.pack(fill=BOTH, expand=1)
-    pt = Table(f, dataframe=df, showstatusbar=True, width=200, height=300)
+    pt = Table(f, dataframe=new_df, showstatusbar=True, width=200, height=300)
     options = {'cellwidth': 150, 'floatprecision': 4, 'align': 'center'}
     config.apply_options(options, pt)
     pt.showIndex()
@@ -79,35 +101,51 @@ def display_all(user_input):
 
 
 # Display GDP of top 10 Countries based on year
-def display_top_gpd(user_input):
-    df.sort_values(by=[user_input, "Country Name"], inplace=True)
-    plt.barh(df['Country Name'][-num:], df[user_input][-num:])
-    plot_design("GPD Of Top Countries")
+def display_top_gdp(user_input):
+    """
+    :param user_input: Fetch year value from user input
+    Generate DataFrame based on factor value and plot accordingly
+    """
+    new_df = trim_column(df, user_input)
+    new_df.sort_values(by=[user_input, "Country Name"], inplace=True)
+    plt.barh(new_df['Country Name'][-num:], new_df[user_input][-num:])
+    plot_design("GDP Of Top Countries")
 
 
 # Display GDP of bottom 10 Countries based on year
-def display_btm_gpd(user_input):
-    df.sort_values(by=[user_input, "Country Name"], inplace=True)
-    print(df[user_input])
-    plt.barh(df['Country Name'][:num], df[user_input][:num])
-    plot_design("GPD Of Bottom Countries")
+def display_btm_gdp(user_input):
+    """
+    :param user_input: Fetch year value from user input
+    Generate DataFrame based on year value and plot accordingly
+    """
+    new_df = trim_column(df, user_input)
+    new_df.sort_values(by=[user_input, "Country Name"], inplace=True)
+    plt.barh(new_df['Country Name'][:num], new_df[user_input][:num])
+    plot_design("GDP Of Bottom Countries")
 
 
 # Display top 10 countries of specific factor in specific year
 def display_specific_factor(user_input, factor_input):
+    """
+    :param user_input: Fetch year value from user input
+    :param factor_input: Fetch factor value from user input
+    Generate DataFrame based on factor value and plot accordingly
+    """
     for file in file_selection:
         if file["Series Name"][0] == factor_input:
             df_2 = pd.DataFrame(file)
             df_2 = clean_dataframe(df_2)
             df_2.sort_values(by=[user_input, "Country Name"], inplace=True)
             plt.barh(df_2['Country Name'][-num:], df_2[user_input][-num:])
+            plt.style.use("fivethirtyeight")
             plot_design(factor_input)
 
 
+"""
+For testing purposes:
 # display_all()
-# display_top_gpd(user_year)
-# display_btm_gpd(user_year)
+# display_top_gdp(user_year)
+# display_btm_gdp(user_year)
 # display_specific_factor(user_year,user_factor)
-
-
 # mainloop()
+"""
